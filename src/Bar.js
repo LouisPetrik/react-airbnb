62baaf6673937eb9951e74ff153a0de07381fdef
LouisPetrik <louispetrik@aol.com>
2019-07-29 19:01:06 +0200

    initial commit

---
+++ src/Bar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Bar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bar.js</h1>
+      </div>
+    );
+  }
+}
